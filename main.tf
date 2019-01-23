aae29695d1f32a65e506a37caff15d15be6903f3
Roger Chao <rogman@gmail.com>
2019-01-23 14:40:34 -0800

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
 provider "nutanix" {}
 
 resource "nutanix_virtual_machine" "vm1" {
-  count = ${var.num}
+  count = ${var.num_of_vms}
   name        = "roger-test-.${count.index}"
 
   num_vcpus_per_socket = 1
