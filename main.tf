de2008594e3abbb19b60026e67a19b8727a510d9
Roger Chao <rogman@gmail.com>
2019-01-23 21:44:55 -0800

    Update main.tf

--- main.tf
+++ main.tf
@@ -4,7 +4,7 @@ resource "nutanix_virtual_machine" "vm1" {
   count       = "${var.num_of_vms}"
   name        = "roger-test-${count.index}"
 
-  num_vcpus_per_socket = 1
+  num_vcpus_per_socket = 2
   num_sockets          = 1
   memory_size_mib      = 2048
 }
